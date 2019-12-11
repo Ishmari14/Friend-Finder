2c5df038508bc159ecfc534610828c9d5192f264
Jhayla Young <jhayla.young@gmail.com>
2019-12-10 18:14:00 -0600

    done with friends.js working on html

--- app/data/friends.js
+++ app/data/friends.js
@@ -11,7 +11,7 @@ var friends = [
         "name": "Janet",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            5, 1, 4, 4, 5, 1, 2, 5, 4, 1
+            2, 1, 4, 3, 5, 1, 2, 2, 1, 1
         ]
     },
 
@@ -19,7 +19,7 @@ var friends = [
         "name": "Suzan",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            5, 1, 4, 4, 5, 1, 2, 5, 4, 1
+            4, 1, 3, 3, 5, 1, 2, 5, 2, 2
         ]
     },
 
@@ -27,7 +27,7 @@ var friends = [
         "name": "Richard",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            5, 1, 4, 4, 5, 1, 2, 5, 4, 1
+            3, 1, 5, 4, 2, 1, 1, 5, 5, 4
         ]
     }
 
